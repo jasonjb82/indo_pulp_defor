d9117abfb5363472767e4f904538b5aac9c98e39
Jason Benedict <jasonjb82@gmail.com>
2021-07-20 12:54:23 -0700

    Update 001_color_palettes.R
    update colours to new classes

--- scripts/001_color_palettes.R
+++ scripts/001_color_palettes.R
@@ -1,22 +1,21 @@
 deft_colors <- c(
   `Never deforested`="#136b3a",
-  `Never deforested, converted to pulp plantation`="#7fad9a",
   `Never deforested, not converted to pulp plantation` = "#136b3a",
-  `Deforestation >3 years before license`="#733f3c",
-  `Deforestation >3 years before license, converted to pulp plantation`="#733f3c",
-  `Deforestation >3 years before license, not converted to pulp plantation`="#EB4A40",
-  `Deforestation in 3 years prior to license`="#cc8037",
-  `Deforestation in 3 years prior to license, converted to pulp plantation`="#cc8037",
-  `Deforestation in 3 years prior to license, not converted to pulp plantation`="#F3AD6A",
-  `Deforestation on licensed concession, before earliest ZDC of downstream mill`="#4b0e82",
-  `Deforestation on licensed concession, before earliest ZDC of downstream mill, converted to pulp plantation`="#4b0e82",
-  `Deforestation on licensed concession, before earliest ZDC of downstream mill, not converted to pulp plantation`="#e30ece",
-  `Deforestation on licensed concession, after earliest ZDC of downstream mill`="#b681e6",
-  `Deforestation on licensed concession, after earliest ZDC of downstream mill, converted to pulp plantation`="#b681e6",
-  `Deforestation on licensed concession, after earliest ZDC of downstream mill, not converted to pulp plantation`="#f299f1",
-  `Deforestation on concession after license`= "#e6e08e",
-  `Deforestation on concession after license, converted to pulp plantation` ="#e6e08e",
-  `Deforestation on concession after license, not converted to pulp plantation`="#d1e820")
+  `Deforestation pre-permit and first planting`="#733f3c",
+  `Deforestation pre-permit and first planting, not converted to pulp plantation`="#733f3c",
+  `Deforestation pre-permit and first planting, converted to pulp plantation`="#EB4A40",
+  `Deforestation post-permit / post-first planting`="#cc8037",
+  `Deforestation post-permit / post-first planting, not converted to pulp plantation`="#cc8037",
+  `Deforestation post-permit / post-first planting, converted to pulp plantation`="#F3AD6A",
+  `Deforestation post-permit / post-first planting and after first ZDC of downstream mill`="#4b0e82",
+  `Deforestation post-permit / post-first planting and after first ZDC of downstream mill, not converted to pulp plantation`="#4b0e82",
+  `Deforestation post-permit / post-first planting and after first ZDC of downstream mill, converted to pulp plantation`="#e30ece")
+  #`Deforestation on licensed concession, after earliest ZDC of downstream mill`="#b681e6",
+  #`Deforestation on licensed concession, after earliest ZDC of downstream mill, converted to pulp plantation`="#b681e6",
+  #`Deforestation on licensed concession, after earliest ZDC of downstream mill, not converted to pulp plantation`="#f299f1",
+  #`Deforestation on concession after license`= "#e6e08e",
+  #`Deforestation on concession after license, converted to pulp plantation` ="#e6e08e",
+  #`Deforestation on concession after license, not converted to pulp plantation`="#d1e820")
 
 deft_cols <- function(...) {
   cols <- c(...)
@@ -36,23 +35,17 @@ add.alpha <- function(col, alpha=1){
 }
 
 cols = c(deft_cols("Never deforested"),
-         deft_cols("Never deforested, converted to pulp plantation"),
          deft_cols("Never deforested, not converted to pulp plantation"),
-         deft_cols("Deforestation >3 years before license"),
-         deft_cols("Deforestation >3 years before license, converted to pulp plantation"),
-         deft_cols("Deforestation >3 years before license, not converted to pulp plantation"),
-         deft_cols("Deforestation in 3 years prior to license"),
-         deft_cols("Deforestation in 3 years prior to license, converted to pulp plantation"),
-         deft_cols("Deforestation in 3 years prior to license, not converted to pulp plantation"),
-         deft_cols("Deforestation on licensed concession, before earliest ZDC of downstream mill"),
-         deft_cols("Deforestation on licensed concession, before earliest ZDC of downstream mill, converted to pulp plantation"),
-         deft_cols("Deforestation on licensed concession, before earliest ZDC of downstream mill, not converted to pulp plantation"),
-         deft_cols("Deforestation on licensed concession, after earliest ZDC of downstream mill"),
-         deft_cols("Deforestation on licensed concession, after earliest ZDC of downstream mill, converted to pulp plantation"),
-         deft_cols("Deforestation on licensed concession, after earliest ZDC of downstream mill, not converted to pulp plantation"),
-         deft_cols("Deforestation on concession after license"),
-         deft_cols("Deforestation on concession after license, converted to pulp plantation"),
-         deft_cols("Deforestation on concession after license, not converted to pulp plantation")
+         deft_cols("Deforestation pre-permit and first planting"),
+         deft_cols("Deforestation pre-permit and first planting, not converted to pulp plantation"),
+         deft_cols("Deforestation pre-permit and first planting, converted to pulp plantation"),
+         deft_cols("Deforestation post-permit / post-first planting"),
+         deft_cols("Deforestation post-permit / post-first planting, not converted to pulp plantation"),
+         deft_cols("Deforestation post-permit / post-first planting, converted to pulp plantation"),
+         deft_cols("Deforestation post-permit / post-first planting and after first ZDC of downstream mill"),
+         deft_cols("Deforestation post-permit / post-first planting and after first ZDC of downstream mill, not converted to pulp plantation"),
+         deft_cols("Deforestation post-permit / post-first planting and after first ZDC of downstream mill, converted to pulp plantation")
+
 )
 
 cols_alpha <- add.alpha(cols, alpha=0.75)
