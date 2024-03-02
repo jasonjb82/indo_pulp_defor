ab5172c3310e5157c4c49901b12fb08d6bb524dc
rheilmayr <rheilmayr@ucsb.edu>
2024-03-01 18:04:52 -0800

    Simplifying script and transitioning output to long format

--- scripts/04_data_analysis/07_calc_harvest_areas_yr_age.R
+++ scripts/04_data_analysis/07_calc_harvest_areas_yr_age.R
@@ -64,25 +64,6 @@ ws <- read_delim(get_object(object="indonesia/wood_pulp/production/out/PULP_WOOD
 
 # supplier groups
 groups <- read_csv(paste0(wdir,"\\01_data\\01_in\\wwi\\ALIGNED_NAMES_GROUP_HTI.csv"))
-
-# wood supply (2020)
-ws_2015_2019 <- read_excel(paste0(wdir,"\\01_data\\01_in\\wwi\\RPBBI_2015_2019_compiled.xlsx")) %>%
-  select(YEAR,SUPPLIER_ID,VOLUME_M3) %>%
-  group_by(YEAR,SUPPLIER_ID) %>%
-  summarize(VOLUME_M3 = sum(VOLUME_M3))
-
-ws_2020 <- read_excel(paste0(wdir,"\\01_data\\01_in\\wwi\\RPBBI_2020_compiled.xlsx")) %>%
-  select(YEAR,SUPPLIER_ID,VOLUME_M3) %>%
-  group_by(YEAR,SUPPLIER_ID) %>%
-  summarize(VOLUME_M3 = sum(VOLUME_M3))
-
-ws_2021 <- read_excel(paste0(wdir,"\\01_data\\01_in\\wwi\\RPBBI_2021_compiled.xlsx")) %>%
-  select(YEAR,SUPPLIER_ID,VOLUME_M3) %>%
-  group_by(YEAR,SUPPLIER_ID) %>%
-  summarize(VOLUME_M3 = sum(VOLUME_M3))
-
-ws_df <- rbind(ws_2015_2019, ws_2020, ws_2021)
-  
   
 # psdh
 psdh <- read_csv(paste0(wdir,"\\01_data\\01_in\\klhk\\psdh\\02_out\\PSDH_HTI_ID_COMBINED.csv"))
@@ -99,7 +80,7 @@ hti_itp_hv <- st_intersection(hti,itp_hv_proj) %>% mutate(area_m2 = st_area(.))
 # create table and clean up
 hti_itp_hv_df <- hti_itp_hv %>%
   st_drop_geometry() %>%
-  select(supplier_id=ID, year=yearint, Class,Harvest1,Harvest2,Harvest3,Ket,area_m2) %>%
+  select(block_id, supplier_id=ID, estab_year=yearint, Class,Harvest1,Harvest2,Harvest3,Ket,area_m2) %>%
   mutate(area_ha = as.double(area_m2*0.0001)) %>%
   select(-area_m2)
 # %>%
@@ -128,29 +109,74 @@ hti_itp_hv_df <- hti_itp_hv_df %>%
 ## Current solution: Assume the establishment year is wrong - use maximum rotation length by reassigning establishment year back to 2000
 ## TODO: double check with David that this is ok
 hti_itp_hv_df <- hti_itp_hv_df %>% 
-  mutate(error_estab = (year>Harvest1) & (Harvest1!=0),
-         year = ifelse(error_estab, 2000, year)) %>% 
+  mutate(error_estab = (estab_year>Harvest1) & (Harvest1!=0),
+         estab_year = ifelse(error_estab, 2000, estab_year)) %>% 
   select(-error_estab)
 
 hti_itp_hv_df <- hti_itp_hv_df %>% 
   mutate(Harvest1 = replace(Harvest1, Harvest1==0, NA),
          Harvest2 = replace(Harvest2, Harvest2==0, NA),
          Harvest3 = replace(Harvest3, Harvest3==0, NA),
-         hv_age1 = Harvest1 - year,
+         hv_age1 = Harvest1 - estab_year,
          hv_age2 = Harvest2 - Harvest1, 
          hv_age3 = Harvest3 - Harvest2)
 
 hti_itp_hv_df_long <- hti_itp_hv_df %>% 
   pivot_longer(cols = starts_with("Harvest"), 
-               names_to = "rotation", values_to = "rotation_length") %>% 
-  select(supplier_id, year, rotation, rotation_length, area_ha)
+               names_to = "rotation", names_prefix = "Harvest", values_to = "harvest_year") %>% 
+  select(block_id, supplier_id, estab_year, rotation, harvest_year, area_ha) %>% 
+  drop_na()
   
+rot_length <- hti_itp_hv_df %>% 
+  pivot_longer(cols = starts_with("hv_age"), 
+               names_to = "rotation", names_prefix = "hv_age", values_to = "rotation_length") %>% 
+  select(block_id, rotation, rotation_length) %>% 
+  drop_na()
 
-hti_itp_hv_df %>% summary()
+fire_flag <- hti_itp_hv_df %>% 
+  mutate(fire_flag = !is.na(Ket)) %>% 
+  select(block_id, fire_flag)
+
+harvest_df <- hti_itp_hv_df_long %>% 
+  left_join(rot_length, by = c("block_id", "rotation")) %>% 
+  left_join(fire_flag, by = "block_id")
 
 
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+## explore rotation lengths -------------------------------------
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+observed_harvests <- harvest_df %>% 
+  filter(rotation>1) %>% 
+  group_by(rotation_length) %>% 
+  summarize(area_sum = sum(area_ha)) %>% 
+  print()
+
+observed_harvests %>% 
+  ggplot(aes(x = rotation_length, y = area_sum)) +
+  geom_line() + 
+  theme_bw()
+
+# Proportion of harvests occurring in specific periods
+total_harvests <- observed_harvests %>% pull(area_sum) %>% sum()
+prop_4_6 <- ((observed_harvests %>% filter(rotation_length >= 4, rotation_length <= 6) %>% pull(area_sum) %>% sum()) / total_harvests) %>% print()
+prop_5 <- ((observed_harvests %>% filter(rotation_length == 5) %>% pull(area_sum) %>% sum()) / total_harvests) %>% print()
+prop_6 <- ((observed_harvests %>% filter(rotation_length <= 6) %>% pull(area_sum) %>% sum()) / total_harvests) %>% print()
+prop_7 <- ((observed_harvests %>% filter(rotation_length <= 7) %>% pull(area_sum) %>% sum()) / total_harvests) %>% print()
 
 
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+## Summarize ha-y harvested in each concession in each year -------------------------------------
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+max_rotation <- 7
+
+concession_harvests <- harvest_df %>% 
+  mutate(impute_flag = (rotation==1) & (rotation_length > 7),
+         rotation_length = ifelse(impute_flag, max_rotation, rotation_length),
+         ha_y = area_ha * rotation_length) %>% 
+  group_by(supplier_id, harvest_year) %>% 
+  summarise(ha_y = sum(ha_y),
+            impute_flag = max(impute_flag),
+            fire_flag = max(fire_flag))
 
 
 # # first year of harvest
@@ -207,6 +233,11 @@ hti_itp_hv_df %>% summary()
 #   select(-VOLUME_M3) %>%
 #   pivot_wider(names_from=TYPE,values_from=c(PC_VOL)) 
 
-## export to csv ---------------------------------------------
-write_csv(hti_harvest_areas_yr_ws,paste0(wdir,"\\01_data\\02_out\\tables\\hti_ws_wood_harvest_yr_age.csv"))
-write_csv(wood_type_hti,paste0(wdir,"\\01_data\\02_out\\tables\\hti_wood_types_psdh.csv"))
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+## Export to csv -------------------------------------
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+write_csv(concession_harvests,paste0(wdir,"\\01_data\\02_out\\tables\\hti_harvest_yr.csv"))
+
+
+# write_csv(hti_harvest_areas_yr_ws,paste0(wdir,"\\01_data\\02_out\\tables\\hti_ws_wood_harvest_yr_age.csv"))
+# write_csv(wood_type_hti,paste0(wdir,"\\01_data\\02_out\\tables\\hti_wood_types_psdh.csv"))
