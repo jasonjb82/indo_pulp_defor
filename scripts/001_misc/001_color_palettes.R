a84746f5e6f51d1c5854028967554f9c3e9796e6
Jason J. Benedict <jasonjb82@gmail.com>
2024-05-03 14:53:59 -0700

    Update 001_color_palettes.R
    cleaning up script

--- scripts/001_misc/001_color_palettes.R
+++ scripts/001_misc/001_color_palettes.R
@@ -1,11 +1,3 @@
-
-# deft_colors <- c(
-#   `Never deforested`="#009E73",
-#   `Deforestation for pulp before first ZDC of downstream mill`="#F0E442",
-#   `Deforestation not for pulp`="#0072B2",
-#   `Deforestation for pulp after first ZDC of downstream mill`="#CC79A7"
-# )
-
 deft_colors <- c(
   `Remaining forest`="#009E73",
   `Deforestation for pulp from 2001-2015`="#F0E442",
@@ -30,32 +22,11 @@ add.alpha <- function(col, alpha=1){
           rgb(x[1], x[2], x[3], alpha=alpha))  
 }
 
-# cols = c(deft_cols("Never deforested"),
-#          deft_cols("Never deforested, not converted to pulp plantation"),
-#          deft_cols("Deforestation pre-permit and first planting"),
-#          deft_cols("Deforestation pre-permit and first planting, not converted to pulp plantation"),
-#          deft_cols("Deforestation pre-permit and first planting, converted to pulp plantation"),
-#          deft_cols("Deforestation post-permit / post-first planting"),
-#          deft_cols("Deforestation post-permit / post-first planting, not converted to pulp plantation"),
-#          deft_cols("Deforestation post-permit / post-first planting, converted to pulp plantation"),
-#          deft_cols("Deforestation post-permit / post-first planting and after first ZDC of downstream mill"),
-#          deft_cols("Deforestation post-permit / post-first planting and after first ZDC of downstream mill, not converted to pulp plantation"),
-#          deft_cols("Deforestation post-permit / post-first planting and after first ZDC of downstream mill, converted to pulp plantation")
-# 
-# )
-# 
-# cols = c(deft_cols("Never deforested"),
-#          deft_cols("Deforestation for pulp before first ZDC of downstream mill"),
-#          deft_cols("Deforestation not for pulp"),
-#          deft_cols("Deforestation for pulp after first ZDC of downstream mill")
-# )
-
 cols = c(deft_cols("Remaining forest"),
          deft_cols("Deforestation for pulp from 2001-2015"),
          deft_cols("Deforestation not for pulp"),
          deft_cols("Deforestation for pulp after 2015")
 )
-
 
 cols_alpha <- add.alpha(cols, alpha=0.85)
 
