1cb9b048efd32d4fd56d82f3d5c146331df89c5d
Jason J. Benedict <jasonjb82@gmail.com>
2026-05-18 16:22:40 -0700

    Update 04_merge_ws_data.R
    update to merge wood supply data from 2015-2022

--- scripts/02_data_cleaning/04_merge_ws_data.R
+++ scripts/02_data_cleaning/04_merge_ws_data.R
@@ -2,7 +2,7 @@
 ## 
 ## Project: Indonesia pulp deforestation
 ##
-## Purpose of script: Merge wood supply data (2015-2019 and 2020 and 2021)
+## Purpose of script: Merge pulpwood supply data (2015-2022)
 ##
 ## Author: Robert Heilmayr and Jason Jon Benedict
 ##
@@ -11,7 +11,7 @@
 ## ---------------------------------------------------------
 ##
 ## Notes: Input datasets
-##        1) HTI concessions (boundaries) and concession start year - from KLHK
+##        1) Pulp mills - Compiled for Trase (by UCSB & WWI)
 ##        2) Wood supply data - RPBBI (cleaned and checked by UCSB & WWI)
 ##
 ## ---------------------------------------------------------
@@ -64,7 +64,6 @@ groups <- read_csv(paste0(wdir,"\\01_data\\01_in\\wwi\\ALIGNED_NAMES_GROUP_HTI.c
 # mills
 mills <- s3read_using(read_excel, object = "indonesia/wood_pulp/logistics/out/mills/MILLS_EXPORTERS_20200405.xlsx", bucket = bucket)
 
-
 ## aggregate data -------------------------------------------
 
 # wood supply (2020)
@@ -79,29 +78,27 @@ ws_2021 <- read_excel(paste0(wdir,"\\01_data\\01_in\\wwi\\RPBBI_2021_compiled.xl
   group_by(YEAR,EXPORTER_ID,SUPPLIER_ID) %>%
   summarize(VOLUME_M3 = sum(VOLUME_M3))
 
-# unqiue mills
+# wood supply (2021)
+ws_2022 <- read_excel(paste0(wdir,"\\01_data\\01_in\\wwi\\RPBBI_2022_compiled.xlsx")) %>%
+  select(YEAR,SUPPLIER_ID,EXPORTER_ID,VOLUME_M3) %>%
+  group_by(YEAR,EXPORTER_ID,SUPPLIER_ID) %>%
+  summarize(VOLUME_M3 = sum(VOLUME_M3))
+
+# pulp mills
 mills_tbl <- mills %>%
   select(MILL_ID,MILL_GROUP,MILL_NAME) %>%
-  distinct()
-
-# unique wood suppliers
-ws_tbl <- ws %>%
-  select(SUPPLIER_ID,SUPPLIER_GROUP,SUPPLIER) %>%
   distinct()
 
 # wood supply by supplier
 ws_all <- ws %>%
   bind_rows(ws_2020) %>%
   bind_rows(ws_2021) %>%
+  bind_rows(ws_2022) %>%
   group_by(YEAR,SUPPLIER_ID,EXPORTER_ID) %>%
   summarize(VOLUME_M3 = sum(VOLUME_M3)) %>%
   left_join(select(groups,SUPPLIER_GROUP=group,SUPPLIER_ID=id,SUPPLIER_NAME=company_clean),by="SUPPLIER_ID") %>%
-  left_join(select(ws_tbl,SUPPLIER_ID,SUPPLIER),by="SUPPLIER_ID") %>%
-  left_join(select(mills_unique,EXPORTER_ID=MILL_ID,EXPORTER_GROUP=MILL_GROUP,MILL_NAME),by="EXPORTER_ID") %>%
-  select(YEAR,SUPPLIER_ID,SUPPLIER_GROUP,SUPPLIER,SUPPLIER_NAME,EXPORTER_ID,EXPORTER_GROUP,MILL_NAME,VOLUME_M3) %>%
-  mutate(SUPPLIER_NAME = ifelse(is.na(SUPPLIER_NAME),SUPPLIER,SUPPLIER_NAME)) %>%
-  select(-SUPPLIER)
-
+  left_join(select(mills_tbl,EXPORTER_ID=MILL_ID,EXPORTER_GROUP=MILL_GROUP,MILL_NAME),by="EXPORTER_ID") %>%
+  select(YEAR,SUPPLIER_ID,EXPORTER_ID,EXPORTER_GROUP,MILL_NAME,VOLUME_M3) 
 
 ## export to csv ---------------------------------------------
-write_csv(ws_all,paste0(wdir,"\\01_data\\02_out\\tables\\ws_merge_clean_2015_2021.csv"))
+write_csv(ws_all,paste0(wdir,"\\01_data\\02_out\\tables\\ws_merge_clean_2015_2022.csv"))
